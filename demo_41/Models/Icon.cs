d790878a2add2a17cca6d4751fd2af29ba6f22a3
phuong2001 <54660042+phuong2001@users.noreply.github.com>
2020-09-10 12:37:51 +0700

    lab7

---
+++ demo_41/Models/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_41.Models
+{
+    public class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
